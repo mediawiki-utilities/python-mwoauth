ceee11b2872bc3493b389c8ae866d205b4710ce7
Bryan Davis <bd808@bd808.com>
2016-10-17 21:47:53 -0600

    Throw typed exceptions rather than Exception
    Catching `Exception` is awkward in application code. Using a local
    exception type allows better scoping of error handling.

--- mwoauth/__init__.py
+++ mwoauth/__init__.py
@@ -3,8 +3,11 @@ OAuth1.0a implementation."""
 from .handshaker import Handshaker
 from .tokens import AccessToken, ConsumerToken, RequestToken
 from .functions import initiate, complete, identify
+from .errors import OAuthException
 from .about import (__name__, __version__, __author__, __author_email__,
                     __description__, __license__, __url__)
+
+from .functions import initiate, complete, identify
 
 
 __all__ = [
@@ -14,6 +17,7 @@ __all__ = [
     Handshaker,
     identify,
     initiate,
+    OAuthException,
     RequestToken,
     __name__, __version__, __author__, __author_email__,
     __description__, __license__, __url__
